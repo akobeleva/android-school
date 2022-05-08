233e8f2dd24cc6561efd784068ee94fa2987ebf1
Анастасия <a.kobeleva@g.nsu.ru>
2022-05-08 21:57:46 +0700

    change layouts

--- app/src/main/java/com/example/androidschool/SearchFragment.kt
+++ app/src/main/java/com/example/androidschool/SearchFragment.kt
@@ -1,7 +1,13 @@
 package com.example.androidschool
 
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ConstraintLayout>(R.id.emptyLayout).visibility = View.VISIBLE
+    }
 }
