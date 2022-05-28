69858e8fe112915f9b48b33f5fa682558836e471
akobeleva <akobeleva@g.nsu.ru>
2022-05-28 13:11:46 +0700

    issue #14 - create MovieConverter

---
+++ app/src/main/java/com/example/androidschool/MovieConverter.kt
@@ -0,0 +1,30 @@
+package com.example.androidschool
+
+import com.example.androidschool.model.dto.Movie
+import com.example.androidschool.model.dto.Poster
+import com.example.androidschool.model.dto.Rating
+import com.example.androidschool.model.entity.MovieEntity
+
+class MovieConverter {
+    fun movieToEntity(movie: Movie): MovieEntity = MovieEntity(
+        movie.id,
+        movie.name,
+        movie.year,
+        movie.description,
+        movie.genres,
+        movie.countries,
+        movie.rating?.kp,
+        movie.poster?.url
+    )
+
+    fun entityToMovie(entity: MovieEntity): Movie = Movie(
+        entity.id,
+        entity.name,
+        entity.year,
+        entity.description,
+        entity.genres,
+        entity.countries,
+        entity.rating?.let { Rating(it) },
+        entity.poster?.let { Poster(it) }
+    )
+}
