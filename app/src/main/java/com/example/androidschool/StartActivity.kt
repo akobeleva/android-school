afdaab602725b326e430bbedca7b603d358cfa91
Анастасия <a.kobeleva@g.nsu.ru>
2022-06-04 22:55:59 +0700

    issue #2 - show StartActivity once

--- app/src/main/java/com/example/androidschool/StartActivity.kt
+++ app/src/main/java/com/example/androidschool/StartActivity.kt
@@ -13,6 +13,7 @@ class StartActivity : AppCompatActivity() {
         findViewById<View>(R.id.startLayout).setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
